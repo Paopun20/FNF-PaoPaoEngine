f737786f9820e3589d34a8a1c46452ce4c044392
Paopun20 <125128258+Paopun20@users.noreply.github.com>
2026-03-19 23:58:05 +0700

    lol

--- setup/unix.sh
+++ setup/unix.sh
@@ -28,6 +28,7 @@ haxelib install hxcpp 4.3.2
 haxelib install tink_core 2.1.1
 haxelib install moonchart 0.5.1
 haxelib install sl-windows-api 1.2.0
+haxelib install random 1.4.1
 haxelib git hython https://github.com/Paopun20/hython.git dev
 haxelib git compiletime https://github.com/Paopun20/compiletime.git
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc.git
