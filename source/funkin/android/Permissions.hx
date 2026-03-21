018d8b3e3bfddbd72843ede5404b167f807afe48
Paopun20 <125128258+Paopun20@users.noreply.github.com>
2026-03-21 15:35:49 +0700

    kesgfkj

--- source/funkin/android/Permissions.hx
+++ source/funkin/android/Permissions.hx
@@ -4,51 +4,6 @@ package funkin.android;
 import com.player03.android6.Permissions as AndroidPermissions;
 #end
 
-#if android
-class PermissionType
-{
-	public static var ACCEPT_HANDOVER = AndroidPermissinos.ACCEPT_HANDOVER;
-	public static var ACCESS_BACKGROUND_LOCATION = AndroidPermissinos.ACCESS_BACKGROUND_LOCATION;
-	public static var ACCESS_COARSE_LOCATION = AndroidPermissinos.ACCESS_COARSE_LOCATION;
-	public static var ACCESS_FINE_LOCATION = AndroidPermissinos.ACCESS_FINE_LOCATION;
-	public static var ACCESS_MEDIA_LOCATION = AndroidPermissinos.ACCESS_MEDIA_LOCATION;
-	public static var ACTIVITY_RECOGNITION = AndroidPermissinos.ACTIVITY_RECOGNITION;
-	public static var ADD_VOICEMAIL = AndroidPermissinos.ADD_VOICEMAIL;
-	public static var ANSWER_PHONE_CALLS = AndroidPermissinos.ANSWER_PHONE_CALLS;
-	public static var BLUETOOTH_ADVERTISE = AndroidPermissinos.BLUETOOTH_ADVERTISE;
-	public static var BLUETOOTH_CONNECT = AndroidPermissinos.BLUETOOTH_CONNECT;
-	public static var BLUETOOTH_SCAN = AndroidPermissinos.BLUETOOTH_SCAN;
-	public static var BODY_SENSORS = AndroidPermissinos.BODY_SENSORS;
-	public static var BODY_SENSORS_BACKGROUND = AndroidPermissinos.BODY_SENSORS_BACKGROUND;
-	public static var CALL_PHONE = AndroidPermissinos.CALL_PHONE;
-	public static var CAMERA = AndroidPermissinos.CAMERA;
-	public static var GET_ACCOUNTS = AndroidPermissinos.GET_ACCOUNTS;
-	public static var NEARBY_WIFI_DEVICES = AndroidPermissinos.NEARBY_WIFI_DEVICES;
-	public static var POST_NOTIFICATIONS = AndroidPermissinos.POST_NOTIFICATIONS;
-	public static var PROCESS_OUTGOING_CALLS = AndroidPermissinos.PROCESS_OUTGOING_CALLS;
-	public static var READ_CALENDAR = AndroidPermissinos.READ_CALENDAR;
-	public static var READ_CALL_LOG = AndroidPermissinos.READ_CALL_LOG;
-	public static var READ_CONTACTS = AndroidPermissinos.READ_CONTACTS;
-	public static var READ_EXTERNAL_STORAGE = AndroidPermissinos.READ_EXTERNAL_STORAGE;
-	public static var READ_MEDIA_AUDIO = AndroidPermissinos.READ_MEDIA_AUDIO;
-	public static var READ_MEDIA_IMAGES = AndroidPermissinos.READ_MEDIA_IMAGES;
-	public static var READ_MEDIA_VIDEO = AndroidPermissinos.READ_MEDIA_VIDEO;
-	public static var READ_PHONE_NUMBERS = AndroidPermissinos.READ_PHONE_NUMBERS;
-	public static var READ_PHONE_STATE = AndroidPermissinos.READ_PHONE_STATE;
-	public static var READ_SMS = AndroidPermissinos.READ_SMS;
-	public static var RECEIVE_MMS = AndroidPermissinos.RECEIVE_MMS;
-	public static var RECEIVE_SMS = AndroidPermissinos.RECEIVE_SMS;
-	public static var RECEIVE_WAP_PUSH = AndroidPermissinos.RECEIVE_WAP_PUSH;
-	public static var RECORD_AUDIO = AndroidPermissinos.RECORD_AUDIO;
-	public static var SEND_SMS = AndroidPermissinos.SEND_SMS;
-	public static var USE_SIP = AndroidPermissinos.USE_SIP;
-	public static var UWB_RANGING = AndroidPermissinos.UWB_RANGING;
-	public static var WRITE_CALENDAR = AndroidPermissinos.WRITE_CALENDAR;
-	public static var WRITE_CALL_LOG = AndroidPermissinos.WRITE_CALL_LOG;
-	public static var WRITE_CONTACTS = AndroidPermissinos.WRITE_CONTACTS;
-	public static var WRITE_EXTERNAL_STORAGE = AndroidPermissinos.WRITE_EXTERNAL_STORAGE;
-}
-#else
 class PermissionType
 {
 	public static var ACCEPT_HANDOVER = "android.permission.ACCEPT_HANDOVER";
@@ -92,7 +47,6 @@ class PermissionType
 	public static var WRITE_CONTACTS = "android.permission.WRITE_CONTACTS";
 	public static var WRITE_EXTERNAL_STORAGE = "android.permission.WRITE_EXTERNAL_STORAGE";
 }
-#end
 
 class Permissions
 {
