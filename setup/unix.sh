6feb26a4bea0d55b52bec04ffdf2bc5e38c7b4c1
Paopun20 <125128258+Paopun20@users.noreply.github.com>
2026-05-07 22:21:23 +0700

    one more

--- setup/unix.sh
+++ setup/unix.sh
@@ -33,13 +33,14 @@ haxelib install sl-windows-api --always
 haxelib git hython https://github.com/Paopun20/hython.git legacy --always # python
 haxelib git compiletime https://github.com/Paopun20/compiletime.git --always
 
-# Cpp Lib
+# Git Lib
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc.git --always
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git --always
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate.git 0188d47c982913eb10fad7bd75f062ddfc680f4b --always
 haxelib git hxhardware https://github.com/Vortex2Oblivion/hxhardware.git --always
 haxelib git lscript https://github.com/SrtHero278/lscript.git --always
 haxelib git lemonui https://github.com/NebulaStellaNova/LemonUI.git --always
+haxelib git nxscript https://github.com/senioritaelizabeth/NxScript.git --always
 
 # funkin
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis.git 22b1ce089dd924f15cdc4632397ef3504d464e90 --always
