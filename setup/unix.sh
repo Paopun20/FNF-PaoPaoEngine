3eda57a5908be5bc68d95ac1a8c221c96ff41c99
Paopun20 <125128258+Paopun20@users.noreply.github.com>
2026-05-21 19:28:22 +0700

    fix build

--- setup/unix.sh
+++ setup/unix.sh
@@ -40,6 +40,7 @@ haxelib git hxhardware https://github.com/Vortex2Oblivion/hxhardware.git --alway
 haxelib git lscript https://github.com/SrtHero278/lscript.git --always
 haxelib git lemonui https://github.com/NebulaStellaNova/LemonUI.git --always
 haxelib git nxscript https://github.com/senioritaelizabeth/NxScript.git --always
+haxelib git flixel-animate https://github.com/MaybeMaru/flixel-animate.git --always
 
 # funkin
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis.git 22b1ce089dd924f15cdc4632397ef3504d464e90 --always
